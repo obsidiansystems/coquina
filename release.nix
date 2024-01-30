805999b97bebb1962a216fd8bdae08ecdb1d9ee2
Ali Abrar <ali.abrar@obsidian.systems>
2024-01-30 12:57:59 -0400

    ci: update nixpkgs

--- release.nix
+++ release.nix
@@ -1,16 +1,16 @@
-{ pkgs ? import ./nixpkgs.nix }:
+{ pkgs ? import ./nixpkgs {} }:
 let
   overrides = self: super: {
     which = self.callHackageDirect {
       pkg = "which";
-      ver = "0.2";
-      sha256 = "1g795yq36n7c6ycs7c0799c3cw78ad0cya6lj4x08m0xnfx98znn";
+      ver = "0.2.0.2";
+      sha256 = "08krfgnjwn9791lwq6azvnj8wy0b1ivyndyhipnrip202vv30rl0";
     } {};
   };
-  targets = ["ghc865" "ghc884"];
+  targets = ["ghc8107" "ghc981"];
 
   ghcs = pkgs.lib.genAttrs targets (target: pkgs.haskell.packages.${target}.override {
     inherit overrides;
   });
 in pkgs.lib.mapAttrs
-    (_: ghc: ghc.callCabal2nix "coquina" ./. {}) ghcs
+    (_: ghc: ghc.callCabal2nix "coquina" (pkgs.lib.cleanSource ./.) {}) ghcs
